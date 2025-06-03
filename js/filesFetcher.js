a6aee11f098946bdccb8d2f739e409b5ce84ac31
Linvin_ <weizhihan3@gmail.com>
2025-06-03 17:50:45 +0200

    Update filesFetcher.js

--- js/filesFetcher.js
+++ js/filesFetcher.js
@@ -1,11 +1,14 @@
 // 负责从 GitHub API 递归抓取所有 .litematic 文件
+const WORKER_PROXY = "https://schema.weizhihan3.workers.dev/";
+
 const API_URL = "https://api.github.com/repos/HT-lab-union/document-of-litematic/contents/contents/schematic/";
 
 let files = [];
 
 async function fetchFiles(url, prefix = "") {
-    const res = await fetch(url);
+    const res = await fetch(WORKER_PROXY + encodeURIComponent(url))
     if (!res.ok) throw new Error("网络错误，无法获取文件列表");
+
     const data = await res.json();
 
     for (const item of data) {
