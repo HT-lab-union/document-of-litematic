d41e22a6f082b467601c681e740d83641ff43517
Linvin_ <weizhihan3@gmail.com>
2025-06-06 23:35:54 +0200

    Update filesFetcher.js

--- js/filesFetcher.js
+++ js/filesFetcher.js
@@ -1,23 +1,16 @@
-// 这里的 URL 是你 CF Workers 的endpoint
-const API_URL = "https://schema.weizhihan3.workers.dev/contents/contents/schematic/";
+async function fetchFileList() {
+  const workerApiUrl = 'https://github-schema.weizhihan3.workers.dev/'; // 你的 Worker URL
+  const res = await fetch(workerApiUrl);
+  if (!res.ok) throw new Error('获取失败');
+  const data = await res.json();
 
-let files = [];
+  // data 是文件数组
+  data.forEach(file => {
+    console.log('文件名:', file.name);
+    console.log('下载链接:', file.download_url);
+  });
 
-async function fetchFiles(url, prefix = "") {
-    const res = await fetch(url);
-    if (!res.ok) throw new Error("网络错误，无法获取文件列表");
-    const data = await res.json();
-
-    for (const item of data) {
-        if (item.type === "dir") {
-            await fetchFiles(item.url, prefix + item.name + "/");
-        } else if (item.name.endsWith(".litematic")) {
-            files.push({
-                name: item.name,
-                path: prefix + item.name,
-                // 如果你想抄:point_down: 将 download_url 替换为你 CF Workers 的代理地址
-                url: `https://schema.weizhihan3.workers.dev/${encodeURIComponent(prefix + item.name)}`
-            });
-        }
-    }
+  return data;
 }
+
+fetchFileList();
