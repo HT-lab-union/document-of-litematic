0e17f839888f7ecb45dbb4fc7d989307e3b8ed6a
Linvin_ <weizhihan3@gmail.com>
2025-06-06 22:52:51 +0200

    Update filesFetcher.js

--- js/filesFetcher.js
+++ js/filesFetcher.js
@@ -1,23 +1,8 @@
-// 这里的 URL 是你 CF Workers 的endpoint
-const API_URL = "https://schema.weizhihan3.workers.dev/contents/schematic/";
-
-let files = [];
-
-async function fetchFiles(url, prefix = "") {
-    const res = await fetch(url);
-    if (!res.ok) throw new Error("网络错误，无法获取文件列表");
-    const data = await res.json();
-
-    for (const item of data) {
-        if (item.type === "dir") {
-            await fetchFiles(item.url, prefix + item.name + "/");
-        } else if (item.name.endsWith(".litematic")) {
-            files.push({
-                name: item.name,
-                path: prefix + item.name,
-                // 如果你想抄👇 将 download_url 替换为你 CF Workers 的代理地址
-                url: `https://schema.weizhihan3.workers.dev/${encodeURIComponent(prefix + item.name)}`
-            });
-        }
-    }
-}
+fetch('https://schema.weizhihan3.workers.dev/') //  Worker 链接
+  .then(res => res.json())
+  .then(files => {
+    files.forEach(file => {
+      console.log(file.name, file.download_url)
+      // 用 file.name 和 file.download_url 生成列表
+    })
+  })
