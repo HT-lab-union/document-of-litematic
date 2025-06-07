d66833c29e103505972aed8e701af18f46686921
Linvin_ <weizhihan3@gmail.com>
2025-06-07 09:08:17 +0200

    Update filesFetcher.js

--- js/filesFetcher.js
+++ js/filesFetcher.js
@@ -1,16 +1,21 @@
-async function fetchFileList() {
-  const workerApiUrl = 'https://github-schema.weizhihan3.workers.dev/'; // 你的 Worker URL
-  const res = await fetch(workerApiUrl);
-  if (!res.ok) throw new Error('获取失败');
-  const data = await res.json();
+const API_URL = "https://github-schema.weizhihan3.workers.dev/contents/contents/schematic/";
 
-  // data 是文件数组
-  data.forEach(file => {
-    console.log('文件名:', file.name);
-    console.log('下载链接:', file.download_url);
-  });
+let files = [];
 
-  return data;
+async function fetchFiles(url, prefix = "") {
+    const res = await fetch(url);
+    if (!res.ok) throw new Error("网络错误，无法获取文件列表");
+    const data = await res.json();
+
+    for (const item of data) {
+        if (item.type === "dir") {
+            await fetchFiles(item.url, prefix + item.name + "/");
+        } else if (item.name.endsWith(".litematic")) {
+            files.push({
+                name: item.name,
+                path: prefix + item.name,
+                url: `https://github-schema.weizhihan3.workers.dev/${encodeURIComponent(prefix + item.name)}`
+            });
+        }
+    }
 }
-
-fetchFileList();
