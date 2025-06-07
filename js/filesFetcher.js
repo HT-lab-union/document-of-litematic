ee18b63072568fa72991567b9360649194a2095b
Linvin_ <weizhihan3@gmail.com>
2025-06-07 12:18:30 +0200

    Update filesFetcher.js

--- js/filesFetcher.js
+++ js/filesFetcher.js
@@ -1,17 +1,20 @@
-let files = [];
+export const API_URL = "https://github-schema.weizhihan3.workers.dev/contents/contents/schematic/";
 
-async function fetchFiles(apiUrl) {
-    const res = await fetch(apiUrl);
+export const files = [];
+
+export async function fetchFiles(url, prefix = "") {
+    const res = await fetch(url);
     const data = await res.json();
 
-    files = [];
-    data.forEach(item => {
-        if (item.name.endsWith('.litematic') || item.name.endsWith('.zip')) {
+    for (const item of data) {
+        if (item.type === "dir") {
+            await fetchFiles(item.url, prefix + item.name + "/");
+        } else if (item.name.endsWith(".litematic") || item.name.endsWith(".zip")) {
             files.push({
                 name: item.name,
-                path: item.name,
-                url: `https://github-schema.weizhihan3.workers.dev/${encodeURIComponent(item.name)}`
+                path: prefix + item.name,
+                url: `https://github-schema.weizhihan3.workers.dev/${encodeURIComponent(prefix + item.name)}`
             });
         }
-    });
+    }
 }
